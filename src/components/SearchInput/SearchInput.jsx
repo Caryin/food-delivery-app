e8d4b2d58bf1e3c62b34807c390eb349da0bfc29
Caryin <yeecaryin@gmail.com>
2024-01-19 18:57:48 +0800

    implement search input

---
+++ src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,15 @@
+import styles from "./SearchInput.module.css";
+import { Search } from "lucide-react";
+
+export const SearchInput = () => {
+  return (
+    <div className={styles["search-box"]}>
+      <Search className={styles["search-icon"]} color="#9b9ea1" size="20px" />
+      <input
+        type="search"
+        placeholder="Enter restaurant name"
+        className={styles["search-input"]}
+      />
+    </div>
+  );
+};
